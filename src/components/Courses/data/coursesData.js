eb7c4d505cbeca83d0fd5689071907f3ded39a22
Zao Qumelashvili <zaoqumelashvili@Zaos-MacBook-Air.local>
2024-01-26 00:51:06 +0400

    Create data file for courses

---
+++ src/components/Courses/data/coursesData.js
@@ -0,0 +1,67 @@
+const courses = [
+  {
+    img: "https://static.wixstatic.com/media/dd97f4_6f93a2a737b14a32ae26b17f51e6dbf1~mv2.jpg/v1/fill/w_300,h_150,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/dd97f4_6f93a2a737b14a32ae26b17f51e6dbf1~mv2.jpg",
+    courseName: "iOS Development",
+    registrationStatus: "false",
+    registrationDate: "",
+    seeMoreSrc: "See Details",
+  },
+  {
+    img: "https://static.wixstatic.com/media/dd97f4_4323db1079614633a653e2ff9a95738e~mv2.jpg/v1/fill/w_600,h_300,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/dd97f4_4323db1079614633a653e2ff9a95738e~mv2.jpg",
+    courseName: "React",
+    registrationStatus: "false",
+    registrationDate: "",
+    seeMoreSrc: "See Details",
+  },
+  {
+    img: "https://static.wixstatic.com/media/dd97f4_5487da3fc590472dbb3599f7575df2d1~mv2.jpg/v1/fill/w_600,h_300,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/dd97f4_5487da3fc590472dbb3599f7575df2d1~mv2.jpg",
+    courseName: "Intro to Python",
+    registrationStatus: "false",
+    registrationDate: "",
+    seeMoreSrc: "See Details",
+  },
+  {
+    img: "https://static.wixstatic.com/media/dd97f4_d9e4b267142542e98dbcdf183d766651~mv2.jpg/v1/fill/w_600,h_300,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/dd97f4_d9e4b267142542e98dbcdf183d766651~mv2.jpg",
+    courseName: "Advnaced Python",
+    registrationStatus: "false",
+    registrationDate: "",
+    seeMoreSrc: "See Details",
+  },
+  {
+    img: "https://static.wixstatic.com/media/dd97f4_41969c5f1e6b4a269bf1d790a52e465b~mv2.jpg/v1/fill/w_600,h_300,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/dd97f4_41969c5f1e6b4a269bf1d790a52e465b~mv2.jpg",
+    courseName: "Advanced Cybersecurity Course",
+    registrationStatus: "false",
+    registrationDate: "",
+    seeMoreSrc: "See Details",
+  },
+  {
+    img: "https://static.wixstatic.com/media/dd97f4_dd355dc915d14264afd4ce2a9d2b0e12~mv2.jpg/v1/fill/w_600,h_300,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/dd97f4_dd355dc915d14264afd4ce2a9d2b0e12~mv2.jpg",
+    courseName: " ToT - Training of Trainers",
+    registrationStatus: "false",
+    registrationDate: "",
+    seeMoreSrc: "See Details",
+  },
+  {
+    img: "https://static.wixstatic.com/media/dd97f4_0fc678e4e2e04f8dacc747a4e2fbb6e6~mv2.jpg/v1/fill/w_600,h_300,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/dd97f4_0fc678e4e2e04f8dacc747a4e2fbb6e6~mv2.jpg",
+    courseName: "Blockchain",
+    registrationStatus: "false",
+    registrationDate: "",
+    seeMoreSrc: "See Details",
+  },
+  {
+    img: "https://static.wixstatic.com/media/dd97f4_dda966974c014319bd5623526510b47c~mv2.jpg/v1/fill/w_600,h_300,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/dd97f4_dda966974c014319bd5623526510b47c~mv2.jpg",
+    courseName: "DevOps",
+    registrationStatus: "false",
+    registrationDate: "",
+    seeMoreSrc: "See Details",
+  },
+  {
+    img: "https://static.wixstatic.com/media/dd97f4_8cc30cf0258d45a6ad2298ebec33550a~mv2.jpg/v1/fill/w_600,h_300,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/dd97f4_8cc30cf0258d45a6ad2298ebec33550a~mv2.jpg",
+    courseName: "Information Security Governance",
+    registrationStatus: "false",
+    registrationDate: "",
+    seeMoreSrc: "See Details",
+  },
+];
+
+export default courses;
