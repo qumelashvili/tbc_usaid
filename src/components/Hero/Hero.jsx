7c9074708cb6f5a539a29e4d4c2f01614a10e3af
Zao Qumelashvili <zaoqumelashvili@Zaos-MacBook-Air.local>
2024-01-25 18:25:45 +0400

    Create css file for Hero, create and add font. style Hero component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,16 @@
 import HeroImg from "./assets/hero_image.jpg";
+import "./Hero.css";
 
 function Hero() {
   return (
-    <div>
+    <div className="hero">
       <img src={HeroImg} alt="" />
-      <div>
+      <div className="hero_text">
         <h1>TBC x USAID</h1>
         <p>ტექნოლოგიური განათლებისთვის</p>
       </div>
     </div>
   );
 }
+
+export default Hero;
