47bc7a15ed2ae4984b70c34581742d6bd55df572
Zao Qumelashvili <zaoqumelashvili@Zaos-MacBook-Air.local>
2024-01-26 00:05:33 +0400

    Create Course card structure

--- src/components/Courses/CoursesCard.jsx
+++ src/components/Courses/CoursesCard.jsx
@@ -1,7 +1,22 @@
-function CoursesCard() {
+function CoursesCard(props) {
   return (
-    <div>
-      <p></p>
+    <div className="card">
+      <img src={props.image} alt="Course image" />
+      <div>
+        <div className="card_info">
+          <h1>{props.courseName}</h1>
+          <p>
+            {props.registrationStatus
+              ? `რეგისტრაცია დასრულდება : ${props.registrationDate}`
+              : "რეგისტრაცია დასრულებულია"}
+          </p>
+        </div>
+
+        <div className="card_link">
+          <i></i>
+          <a href="">კურსის დეტალები</a>
+        </div>
+      </div>
     </div>
   );
 }
