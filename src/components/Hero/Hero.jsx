eb9c14c7cdc5a79cadce823083ead8a215a4bbb3
Zao Qumelashvili <zaoqumelashvili@Zaos-MacBook-Air.local>
2024-01-25 18:07:36 +0400

    Create File for Hero, insert assets

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import HeroImg from "./assets/hero_image.jpg";
+
+function Hero() {
+  return (
+    <div>
+      <img src={HeroImg} alt="" />
+      <div>
+        <h1>TBC x USAID</h1>
+        <p>ტექნოლოგიური განათლებისთვის</p>
+      </div>
+    </div>
+  );
+}
